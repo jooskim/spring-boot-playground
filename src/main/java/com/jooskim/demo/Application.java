1a47fd166587786d5835e8ff1e27e62b9e78217b
Josh Kim <kjosh@vmware.com>
2018-10-21 22:12:24 -0500

    Multi environments

--- src/main/java/com/jooskim/demo/Application.java
+++ src/main/java/com/jooskim/demo/Application.java
@@ -1,12 +1,28 @@
 package com.jooskim.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Value(value = "${environment}")
+	public String env;
+
+	@Bean
+	public CommandLineRunner printOutEnv() {
+		return (args) -> {
+			log.info(env);
+		};
 	}
 }
